147cf96b0f21c7f1b9d5a5027aea4bc99b401590
BelchiorQS <belchiorqs@gmail.com>
2024-10-19 20:47:35 +0100

    strutura Swich

---
+++ C/beginner/A14.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int day = 2;
+
+    switch (day)
+    {
+    case 1:
+        printf("Monday");
+        break;
+    case 2:
+        printf("Tuesday");
+        break;
+    case 3:
+        printf("Wednesday");
+        break;
+    case 4:
+        printf("Thursday");
+        break;
+    case 5:
+        printf("Friday");
+        break;
+    case 6:
+        printf("Saturday");
+        break;
+    case 7:
+        printf("Sunday");
+        break;
+    default:
+        printf("dia inexistente!");
+    }
+    return 0;
+}
