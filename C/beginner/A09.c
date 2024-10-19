c2b33ff77a6485f18cb1c653293205acc7a3587e
BelchiorQS <belchiorqs@gmail.com>
2024-10-19 15:39:22 +0100

    Exemplo de vida real

---
+++ C/beginner/A09.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void)
+{
+    // Set the maximum possible score in the game to 500
+    int maxScore = 500;
+
+    // The actual score of the user
+    int userScore = 423;
+
+    /* Calculate the percantage of the user's score in relation to the maximum available score.
+    Convert userScore to float to make sure that the division is accurate */
+    float percentage = (float)userScore / maxScore * 100.0;
+
+    // Print the percentage
+    printf("User's percentage is %.2f", percentage);
+    return 0;
+}
