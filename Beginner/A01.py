97bb5c68fe46ceb6013d59751ff4347e75f1d0dc
BelchiorQS <belchiorqs@gmail.com>
2024-10-19 11:37:10 +0100

    reorganizar

--- Beginner/A01.py
+++
@@ -1,3 +0,0 @@
-if 5 > 2:
-    print(" 5 maior que 2")
-# essa e a forma correta de se identar uum codigo
--- Beginner/A02.py
+++
@@ -1,5 +0,0 @@
-print("Hello, my name is Belchior Soares") #Esse umm comentario em uma linha
-
-"""Aqu e um comentario 
-    de multilinha
-"""
