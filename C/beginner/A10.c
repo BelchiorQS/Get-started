a9e98f8626661ace105e21ccfdc1f6642bda783e
BelchiorQS <belchiorqs@gmail.com>
2024-10-19 18:15:54 +0100

    Constantes

---
+++ C/beginner/A10.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void)
+{
+    const int minutesPerHour = 60;
+    const float PI = 3.14;
+
+    printf("%i",minutesPerHour);
+    printf("\t%.2f",PI);
+    return 0;
+}
