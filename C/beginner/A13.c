1f7ac9f6496561f296ee4ec026c9503d9adcc494
BelchiorQS <belchiorqs@gmail.com>
2024-10-19 20:27:14 +0100

    estrutura de condicao IF,ELSE

---
+++ C/beginner/A13.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int x = 20;
+    int y = 18;
+
+    if (x < y)
+        printf("Good day.");
+    else
+        printf("Good evening.");
+    return 0;
+}
